c468748f9414cba703dbc6a4f81079f5b2e9bed9
tall-horse <dima.gamedev@pm.me>
2025-06-11 12:33:10 +0200

    Created a hotel booking controller

---
+++ HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public HotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
