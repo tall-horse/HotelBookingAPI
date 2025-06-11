363d664d7d93c4d79c8abfc38231aaa826384bef
tall-horse <dima.gamedev@pm.me>
2025-06-11 12:30:39 +0200

    Setting up a database

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI;
+using HotelBookingAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApiContext>
+    (opt => opt.UseInMemoryDatabase("BookingDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
